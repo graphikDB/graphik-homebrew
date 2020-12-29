deb604cff28a2b9d960561eb230893382d290867
autom8ter <colemanword@gmail.com>
2020-12-28 22:13:59 -0700

    fix sha

--- Formula/graphik.rb
+++ Formula/graphik.rb
@@ -2,7 +2,7 @@ class Graphik < Formula
   version 'v1.0.0'
   homepage 'https://github.com/graphikDB/graphik/blob/main/README.md'
   url "https://github.com/graphikDB/graphik/releases/download/v1.0.0/graphik_darwin_amd64"
-  sha256 'f5f96bd919efa1258c331079f3f75a0d44f2d2aa7ec9d62926d1a6af3decb3a3'
+  sha256 '1172bb0b5948bf76f79a8b61a23996ec2db635a01687656badfc601ce6ce29a9'
   desc 'Graphik is a Backend as a Service implemented as an identity-aware, permissioned, persistant document/graph database & pubsub server written in Go.'
 
   def install
