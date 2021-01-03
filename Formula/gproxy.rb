2bcdcd44d9af428c1caa1fdad2251d4fb2fcee6f
autom8ter <colemanword@gmail.com>
2021-01-03 16:29:32 -0700

    gproxy 1.0.0

--- Formula/gproxy.rb
+++ Formula/gproxy.rb
@@ -1,8 +1,8 @@
 class Gproxy < Formula
-  version 'v0.0.17'
+  version 'v1.0.0'
   homepage 'https://github.com/graphikDB/gproxy/blob/main/README.md'
-  url "https://github.com/graphikDB/gproxy/releases/download/v0.0.17/gproxy_darwin_amd64"
-  sha256 '0d5054c626592f97480701089285e8dc5b43627435e78764c792e35536fad986'
+  url "https://github.com/graphikDB/gproxy/releases/download/v1.0.0/gproxy_darwin_amd64"
+  sha256 '091f3aa8cb6860dd97842e104547866d36ff5325434b3b5ffba5684d0ff489d2'
   desc 'gproxy is a tiny service/library for creating lets-encrypt/acme secured gRPC and http reverse proxies'
 
   def install
